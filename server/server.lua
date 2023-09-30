22dc592c9dd3e40c46eda88afdd80ad07daf341b
RexshackGaming <rexshackuk@gmail.com>
2023-09-30 13:32:23 +0100

    version checker

--- server/server.lua
+++ server/server.lua
@@ -1,6 +1,35 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
---------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------
+-- version checker
+-----------------------------------------------------------------------
+local function versionCheckPrint(_type, log)
+    local color = _type == 'success' and '^2' or '^1'
+
+    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+end
+
+local function CheckVersion()
+    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-hotel/main/version.txt', function(err, text, headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+
+        if not text then 
+            versionCheckPrint('error', 'Currently unable to run a version check.')
+            return 
+        end
+
+        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
+        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
+        
+        if text == currentVersion then
+            versionCheckPrint('success', 'You are running the latest version.')
+        else
+            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
+        end
+    end)
+end
+
+-----------------------------------------------------------------------
 
 -- rent room
 RegisterNetEvent('rsg-hotel:server:RentRoom', function(data)
@@ -169,3 +198,8 @@ SetTimeout(Config.BillingCycle * (60 * 60 * 1000), BillingInterval) -- hours
 
 --------------------------------------------------------------------------------------------------
 --SetTimeout(Config.BillingCycle * (60 * 1000), BillingInterval) -- mins
+
+--------------------------------------------------------------------------------------------------
+-- start version check
+--------------------------------------------------------------------------------------------------
+CheckVersion()
